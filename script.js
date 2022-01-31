b71401d032a9227fb8f366d9e7358b8bff57aa51
maykkk1 <maycon5591@gmail.com>
2022-01-31 11:56:03 -0300

    regra de negocio ajustada

--- script.js
+++ script.js
@@ -1,73 +1,94 @@
+const custom = document.getElementById('custom');
+const numOfPeopleActivate = document.getElementById('number-of-people');
+const billActivate = document.getElementById('bill-value');
+let percentageBtn = 0
 const btn5 = document.getElementById('btn-5%');
 const btn10 = document.getElementById('btn-10%');
 const btn15 = document.getElementById('btn-15%');
 const btn25 = document.getElementById('btn-25%');
 const btn50 = document.getElementById('btn-50%');
-const custom = document.getElementById('custom');
-const numOfPeopleActivate = document.getElementById('number-of-people');
-const billActivate = document.getElementById('bill-value');
+
+
+
 
 btn5.addEventListener('click', function (event) {
-    calculate(5)
+    changePercentageBtn(5)
 });
+btn5.addEventListener('click', calculate)
+
 
 btn10.addEventListener('click', function (event) {
-    calculate(10)
+    changePercentageBtn(10)
 });
+btn10.addEventListener('click', calculate)
+
 
 btn15.addEventListener('click', function (event) {
-    calculate(15)
+    changePercentageBtn(15)
 });
+btn15.addEventListener('click', calculate)
+
 
 btn25.addEventListener('click', function (event) {
-    calculate(25)
+    changePercentageBtn(25)
 });
+btn25.addEventListener('click', calculate)
+
 
 btn50.addEventListener('click', function (event) {
-    calculate(50)
+    changePercentageBtn(50)
 });
+btn50.addEventListener('click', calculate)
 
 
-custom.addEventListener('change', calculateWithNumberOfPeopleActivate);
+function changePercentageBtn (percentage) {
+    percentageBtn = percentage
+    resetBtnBackground()
+    document.getElementById(`btn-${percentage}%`).style.backgroundColor= "hsl(172, 67%, 45%)";
+    document.getElementById(`btn-${percentage}%`).style.color= "hsl(183, 100%, 15%)";
+}
 
-numOfPeopleActivate.addEventListener('change', calculateWithNumberOfPeopleActivate);
-
-billActivate.addEventListener('change', calculateWithNumberOfPeopleActivate);
-
-function calculate(percentage) {
-    document.getElementById('custom').value = 'custom'
-    const numeroDePessoas = parseInt(document.getElementById('number-of-people').value);
-    const bill = parseFloat(document.getElementById("bill-value").value);
-    if (numeroDePessoas == 0) {
-        document.getElementById('cant-be-zero').style.display = 'block';
-        document.getElementById("number-of-people").style.border = "1px solid red";
-    } else {
-        if (isNaN(bill) || isNaN(numeroDePessoas) || isNaN(percentage)) {
-            document.getElementById("number-of-people").style.border = "none";
-            document.getElementById('cant-be-zero').style.display = 'none';
-        } else {
-            document.getElementById("number-of-people").style.border = "none";
-            document.getElementById('cant-be-zero').style.display = 'none';
-            const total = (((percentage * bill) / 100) + bill) / numeroDePessoas
-            const tipAmount = ((percentage * bill) / 100) / numeroDePessoas
-            document.getElementById('total-value').innerHTML = `$${total.toFixed(2)}`
-            document.getElementById('tip-amount-value').innerHTML = `$${tipAmount.toFixed(2)}`
-        }
-    }
+function resetBtnBackground () {
+    document.getElementById('btn-5%').style.backgroundColor= 'hsl(183, 100%, 15%)';
+    document.getElementById('btn-5%').style.color= 'white';
+    document.getElementById('btn-10%').style.backgroundColor= 'hsl(183, 100%, 15%)';
+    document.getElementById('btn-10%').style.color= 'white';
+    document.getElementById('btn-15%').style.backgroundColor= 'hsl(183, 100%, 15%)';
+    document.getElementById('btn-15%').style.color= 'white';
+    document.getElementById('btn-25%').style.backgroundColor= 'hsl(183, 100%, 15%)';
+    document.getElementById('btn-25%').style.color= 'white';
+    document.getElementById('btn-50%').style.backgroundColor= 'hsl(183, 100%, 15%)';
+    document.getElementById('btn-50%').style.color= 'white';
 }
 
 
-function calculateWithNumberOfPeopleActivate() {
+
+
+function setToZero () {
+    percentageBtn = 0
+    resetBtnBackground()
+}
+
+custom.addEventListener('change', setToZero);
+custom.addEventListener('change', calculate);
+numOfPeopleActivate.addEventListener('change', calculate);
+billActivate.addEventListener('change', calculate);
+
+
+function calculate() {
+    document.getElementById('btn-reset').style.backgroundColor ='hsl(172, 67%, 45%)'
+    let percentage = parseInt(document.getElementById('custom').value);
     const numeroDePessoas = parseInt(document.getElementById('number-of-people').value);
     const bill = parseFloat(document.getElementById("bill-value").value);
-    const percentage = parseInt(document.getElementById('custom').value);
+    if (percentageBtn != 0) {
+        percentage = percentageBtn
+    }
     if (numeroDePessoas == 0) {
         document.getElementById('cant-be-zero').style.display = 'block';
         document.getElementById("number-of-people").style.border = "1px solid red";
     } else { if (isNaN(bill) || isNaN(percentage) || isNaN(numeroDePessoas)) {
         document.getElementById("number-of-people").style.border = "none";
         document.getElementById('cant-be-zero').style.display = 'none';
-        pass
     } else {
         document.getElementById("number-of-people").style.border = "none";
         document.getElementById('cant-be-zero').style.display = 'none';
@@ -79,15 +100,11 @@ function calculateWithNumberOfPeopleActivate() {
     }
 }
 
-
-
-
-
 const btnReset = document.getElementById('btn-reset');
-
 btnReset.addEventListener('click', reset)
-
 function reset() {
+    resetBtnBackground()
+    document.getElementById('btn-reset').style.backgroundColor ='hsl(183, 94%, 20%)'
     document.getElementById("number-of-people").style.border = "none";
     document.getElementById('cant-be-zero').style.display = 'none';
     document.getElementById('total-value').innerHTML = '$0.00'
@@ -96,6 +113,15 @@ function reset() {
     document.getElementById("bill-value").value = ''
     document.getElementById('custom').value = 'custom'
 }
+
+
+
+
+
+
+
+
+
 
 
 
